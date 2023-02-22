88ee49c9588c99120e017af0c0bf838a8a594255
Lokeshwar Kumar Tabjula <lk544219@dal.ca>
2023-02-21 22:02:58 -0400

    test: initialSetup and test cases

---
+++ q55/minStackImpl/src/main/java/com/pair25/minStackImpl/MinStackImplApplication.java
@@ -0,0 +1,13 @@
+package com.pair25.minStackImpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinStackImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinStackImplApplication.class, args);
+	}
+
+}
---
+++ q55/minStackImpl/src/test/java/com/pair25/minStackImpl/MinStackImplApplicationTests.java
@@ -0,0 +1,45 @@
+package com.pair25.minStackImpl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinStackImplApplicationTests {
+	
+	@InjectMocks
+	MinStackImplApplication minStackImpl;
+	
+	
+	@Test
+	public void testStackPushPositiveCase1() {
+		MinStack minStack = new MinStack();
+		minStack.push(4);
+		int arr[] =   minStack.getStk();
+		Assertions.assertEquals(arr[0], 4);
+	}
+	
+	@Test
+	public void testStackPopPositiveCase1() {
+		MinStack minStack = new MinStack();
+		minStack.push(4);
+		minStack.push(5);
+		int minValue = minStack.pop();
+		Assertions.assertEquals(minValue, 4);
+	}
+	
+	@Test
+	public void testStackTopPositiveCase1() {
+		MinStack minStack = new MinStack();
+		minStack.push(4);
+		minStack.push(5);
+		int topValue = minStack.top();
+		Assertions.assertEquals(topValue, 5);
+	}
+	
+	S
+	
+
+}
