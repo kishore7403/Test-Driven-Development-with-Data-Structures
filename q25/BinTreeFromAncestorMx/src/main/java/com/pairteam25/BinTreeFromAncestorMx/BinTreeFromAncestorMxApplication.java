33d9fdb2992f302e8568ffabf515c3f7decc47fb
Lokeshwar Kumar Tabjula <lk544219@dal.ca>
2023-02-27 20:10:02 -0400

    feat: adding one hardcoded user input case in main

--- q25/BinTreeFromAncestorMx/src/main/java/com/pairteam25/BinTreeFromAncestorMx/BinTreeFromAncestorMxApplication.java
+++ q25/BinTreeFromAncestorMx/src/main/java/com/pairteam25/BinTreeFromAncestorMx/BinTreeFromAncestorMxApplication.java
@@ -3,10 +3,26 @@ package com.pairteam25.BinTreeFromAncestorMx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BinTreeFromAncestorMxApplication {
 
 	public static void main(String[] args) {
+		
+
+		boolean[][] ancestorMatrix = {                
+				{ false, false, false, false, false, false }, 
+				{ true, false, false, false, true, false },              
+				{ false, false, false, true, false, false }, 
+				{ false, false, false, false, false, false },           
+				{ false, false, false, false, false, false }, 
+				{ true, true, true, true, true, false }        
+				};       
+		Node root = TreeUtilities.constructTree(ancestorMatrix);       
+		TreeUtilities.inorderTraverse(root);       
+		System.out.println(TreeUtilities.displayTree());
+		
+		
 		SpringApplication.run(BinTreeFromAncestorMxApplication.class, args);
 	}
 
